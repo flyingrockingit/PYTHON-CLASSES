4da066233ebd2eea1631b44aa473b49166ef2990
flyingrockingit <coolavniroy@gmail.com>
2025-08-19 11:30:25 +0300

    Add files via upload

---
+++ main2(project).py
@@ -0,0 +1,9 @@
+a = int(input("Enter first number (a): "))
+b = int(input("Enter second number (b): "))
+c = int(input("Enter third number (c): "))
+
+print("\nBefore swapping:")
+print(f"a = {a}, b = {b}, c = {c}")
+
+print("\nAfter swapping:")
+print(f"a = {a}, b = {b}, c = {c}")
